31f75542270afc446ca3a95e23dd5a27b8dc541e
rowidsayyed <rowidsayyed@gmail.com>
2020-02-04 22:47:59 +0200

    add Javascript

---
+++ css/JAV.js
@@ -0,0 +1,10 @@
+var person = prompt("Please enter your name", "Harry Potter");
+var txt;
+alert("heyyyyy");
+var r = confirm("Press a button!");
+if (r == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
+document.write('<h3>'+txt+'</h3>');
