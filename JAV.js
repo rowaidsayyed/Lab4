ce278384193e65fb5d32d2417b1d8006f2929125
rowidsayyed <rowidsayyed@gmail.com>
2020-02-04 23:51:47 +0200

    update JS

--- JAV.js
+++ JAV.js
@@ -1,11 +1,14 @@
-
-alert("heyyyyy");
-var person = prompt("Please enter your name", "Harry Potter");
 var txt;
-var r = confirm("Press a button!");
-if (r == true) {
+var major;
+var check ;
+check= confirm("Press a button!");
+setTimeout(function(){ alert("Welcome 102d5");}, 1500);
+setTimeout(function(){ prompt("Please enter your major");}, 3000);
+
+if (check == true) {
   txt = "You pressed OK!";
 } else {
   txt = "You pressed Cancel!";
 }
+
 document.write('<h3>'+txt+'</h3>');
