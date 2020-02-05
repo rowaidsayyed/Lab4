54b0216251613cdba5107eb751b4760e263dfca5
rowidsayyed <rowidsayyed@gmail.com>
2020-02-05 19:29:26 +0200

    update gavascript file

--- JAV.js
+++ JAV.js
@@ -1,32 +1,12 @@
-function three_in_one(){
-  var txt;
-  var major;
-  var check ;
-  var name ;
+check= confirm("Press a button!");
+var name =prompt("Please enter your name",'102 programmer');
+setTimeout(function(){ alert("Welcome 102d5");}, 1500);
+setTimeout(function(){ prompt("Please enter your major");}, 3000);
+    
+if (check == true) 
+{   txt = "You pressed OK!"; }
+else 
+{   txt = "You pressed Cancel!"; }
 
-  check= confirm("Press ok to continue to ");
-  if (check == true) {
-    alert("Welcome 102d5");
-    name =prompt("Please enter your name",'ahmad');
-    major=prompt("Please enter your major");  
-    setTimeout(function(){ alert("Thank you");}, 2500);  
-  }
-  
- 
-  if (check == true) 
-  {   txt = "You pressed OK!"; }
-  else 
-  {   txt = "You pressed Cancel!"; }
-
-  if (check == true && name !=null) 
-  {  name= name+' ' + major; }
-
-  return [txt,name]; 
-}
-
-
-var txt;
-var name; 
-[txt,name]=three_in_one();
 document.write('<h3>'+"Helo "+name +'</h3>');
 document.write('<h3>'+txt+'</h3>');
