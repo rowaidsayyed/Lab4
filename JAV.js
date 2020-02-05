81b0c1e4caf37f5025f2be27167df9db27ad5ad2
rowidsayyed <rowidsayyed@gmail.com>
2020-02-05 13:22:03 +0200

    try

--- JAV.js
+++ JAV.js
@@ -2,21 +2,24 @@ function three_in_one(){
   var txt;
   var major;
   var check ;
+  var name ;
 
-  check= confirm("Press a button!");
-  var name =prompt("Please enter your name");
-  setTimeout(function(){ alert("Welcome 102d5");}, 2500);
-  alert("be attention");
-  major=prompt("Please enter your major");
-  setTimeout(function(){ alert("Thank you");}, 3500);
-
+  check= confirm("Press ok to continue to ");
+  if (check == true) {
+    alert("Welcome 102d5");
+    name =prompt("Please enter your name",'ahmad');
+    major=prompt("Please enter your major");  
+    setTimeout(function(){ alert("Thank you");}, 2500);  
+  }
+  
+ 
   if (check == true) 
   {   txt = "You pressed OK!"; }
   else 
   {   txt = "You pressed Cancel!"; }
 
   if (check == true && name !=null) 
-  {  name= name+ major; }
+  {  name= name+' ' + major; }
 
   return [txt,name]; 
 }
